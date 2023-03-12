a1db442962b46f79587b249391ed18d219f2e0be
Phumiphat Meemano <hero.phumiphat@gmail.com>
2023-03-12 13:07:02 +0700

    clear project

--- README.md
+++
@@ -1,5 +0,0 @@
-# react-redux-toolkit
-
-กำลังพัฒนา Project...
-
-https://phenomenal-scone-f3403e.netlify.app/
--- src/components/pages/Cart.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Cart() {
-    return (
-        <div>
-            <h1>Cart</h1>
-        </div>
-    )
-}
